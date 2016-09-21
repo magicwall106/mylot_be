8615c4d7c12c0be20f211a6c027fcf7631a158ae
tuong le <magicwall106@gmail.com>
2016-09-22 00:24:09 +0700

    Set Header Allow some method

--- app.js
+++ app.js
@@ -113,6 +113,7 @@ app.use(function (req, res, next) {
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Allow","GET, HEAD, POST, PUT, DELETE");
   next();
 });
 
